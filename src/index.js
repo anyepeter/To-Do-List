e972ad6b29e82b0d8a3e54fa57097ff0c5598137
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-10-07 12:01:54 +0100

    Import the interactive function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import './style.css';
 import addFunction from './modules/addItem.js';
 import renderItems from './modules/addItems.js';
+import completedAll from './modules/interactive.js';
 
 addFunction();
 renderItems();
+completedAll();
 
-export default { addFunction, renderItems };
+export default { addFunction, renderItems, completedAll };
